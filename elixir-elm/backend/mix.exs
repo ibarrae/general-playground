5f86ad166e11db2eb6e3987883bfbad9e4444f43
Esteban Ibarra <eibarra1992@hotmail.com>
2020-02-20 16:14:52 -0500

    Add basic auth dependency

--- elixir-elm/backend/mix.exs
+++ elixir-elm/backend/mix.exs
@@ -43,7 +43,8 @@ defmodule Backend.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:bcrypt_elixir, "~> 2.1.0"}
+      {:bcrypt_elixir, "~> 2.1.0"},
+      {:basic_auth, "~> 2.2.3"}
     ]
   end
 
