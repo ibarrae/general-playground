8d5c76220539e4868b9ae149e2cff6acf33b1fc8
Esteban Ibarra <eibarra1992@hotmail.com>
2020-02-21 10:52:03 -0500

    Set-up of JTW serializing

--- elixir-elm/backend/config/config.exs
+++ elixir-elm/backend/config/config.exs
@@ -31,3 +31,9 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+# JWT config
+config :guardian, Guardian,
+  issuer: "eibarra-test",
+  secret_key: Mix.env(),
+  serializer: Backend.JWTSerializer
--- elixir-elm/backend/mix.exs
+++ elixir-elm/backend/mix.exs
@@ -20,8 +20,7 @@ defmodule Backend.MixProject do
   def application do
     [
       mod: {Backend.Application, []},
-      extra_applications: [:logger, :runtime_tools],
-      applications: [:guardian]
+      extra_applications: [:logger, :runtime_tools, :guardian]
     ]
   end
 
