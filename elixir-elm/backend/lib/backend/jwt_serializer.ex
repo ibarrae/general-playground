aa54334ba8593bf75e8920290b21b728107ffcec
Esteban Ibarra <eibarra1992@hotmail.com>
2020-02-21 11:51:45 -0500

    Use constant for unknown_resource

--- elixir-elm/backend/lib/backend/jwt_serializer.ex
+++ elixir-elm/backend/lib/backend/jwt_serializer.ex
@@ -4,9 +4,11 @@ defmodule Backend.JWTSerializer do
   alias Backend.Repo
   alias Backend.Auth.User
 
+  @unknown_resource {:error, "Unknown resource type"}
+
   def subject_for_token(user = %User{}, _claims), do: {:ok, "User:#{user.id}"}
-  def subject_for_token(_, _), do: {:error, "Unknown resource type"}
+  def subject_for_token(_, _), do: @unknown_resource
 
   def resource_from_claims("User:" <> id), do: {:ok, Repo.get(User, id)}
-  def resource_from_claims(_), do: {:error, "Unknown resource type"}
+  def resource_from_claims(_), do: @unknown_resource
 end
