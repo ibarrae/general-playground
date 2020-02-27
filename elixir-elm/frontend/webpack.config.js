7c645cd0e0ef6af4572d2afbd095a5999034234f
Esteban Ibarra <eibarra1992@hotmail.com>
2020-02-27 11:11:50 -0500

    Add debug for elm

--- elixir-elm/frontend/webpack.config.js
+++ elixir-elm/frontend/webpack.config.js
@@ -1,49 +1,52 @@
-const path = require('path');
+const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-const webpack = require('webpack');
-const env = require('dotenv').config();
+const webpack = require("webpack");
+const env = require("dotenv").config();
 
 module.exports = {
   entry: {
-    app: [
-      './src/index.js'
-    ]
+    app: ["./src/index.js"]
   },
 
   output: {
-    path: path.resolve(__dirname + '/dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname + "/dist"),
+    filename: "[name].js"
   },
 
   module: {
     rules: [
       {
-        test:    /\.html$/,
+        test: /\.html$/,
         exclude: /node_modules/,
-        loader:  'html-loader',
+        loader: "html-loader"
       },
       {
-        test:    /\.elm$/,
+        test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader:  'elm-webpack-loader?verbose=true',
-      },
+        use: [
+          {
+            loader: "elm-webpack-loader",
+            options: { verbose: true, debug: true }
+          }
+        ]
+      }
     ],
 
-    noParse: /\.elm$/,
+    noParse: /\.elm$/
   },
 
   plugins: [
     new HTMLWebpackPlugin({
-        template: "src/index.html",
-        inject: "body"
+      template: "src/index.html",
+      inject: "body"
     }),
     new webpack.DefinePlugin({
       API_ROOT: JSON.stringify(env.parsed.API_ROOT)
     })
-],
+  ],
 
   devServer: {
     inline: true,
-    stats: { colors: true },
-  },
+    stats: { colors: true }
+  }
 };
